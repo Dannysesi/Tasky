c6ec6d0258d5180a8157b9f61b7859f7582b30fd
Daniel Olusesi <116007382+Dannysesi@users.noreply.github.com>
2025-08-13 13:40:43 +0100

    Update settings.py

--- task/settings.py
+++ task/settings.py
@@ -165,6 +165,7 @@ CLOUDINARY_STORAGE = {
 
 cloudinary.config(
     cloud_name = os.environ.get('CLOUD_NAME'),
+    # Update API Key
     api_key = os.environ.get('API_KEY'),
     api_secret = os.environ.get('API_SECRET')
 ,
